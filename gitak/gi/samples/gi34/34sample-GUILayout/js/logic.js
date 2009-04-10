2b77366decdfea433221683d40bf05b127e2e1f9
tibco <tibco@21de6678-13f1-11de-81b6-69408224c2c7>
2009-04-10 06:44:19 +0000

    Importing GITAK trunk (0.9)

---
+++ gitak/gi/samples/gi34/34sample-GUILayout/js/logic.js
@@ -0,0 +1,14 @@
+jsx3.lang.Package.definePackage(
+  "eg.guilayout",                //the full name of the package to create
+  function(guilayout) {          //name the argument of this function
+    /**
+     * Load componets at runtime on to a pane.
+     * @param componentURL {String} relative path of canvas/component URL.
+     */    
+     guilayout.loadCanvas = function (objJSX, componentURL){
+         var contentPane = objJSX.getServer().getJSXByName('blkContentPane');
+         //unload the existing canvas
+         contentPane.removeChildren();
+         contentPane.load(componentURL, true);   
+     }
+});
