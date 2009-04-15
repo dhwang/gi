aa3547371d34ab3f23d967ee8a60b1b286920d8e
dhwang <dhwang@21de6678-13f1-11de-81b6-69408224c2c7>
2009-04-15 21:10:38 +0000

    ------------------------------------------------------------------------ r5892 | dhwang | 2008-10-07 19:49:44 -0700 (Tue, 07 Oct 2008) | 1 line
    Fix the sample to work in Firefox.
    ------------------------------------------------------------------------

--- gitak/gi/samples/gi34/34sample-searchInList/js/logic.js
+++ gitak/gi/samples/gi34/34sample-searchInList/js/logic.js
@@ -4,7 +4,7 @@ jsx3.lang.Package.definePackage(
   /**
    * Position of the record in the CDF document to be returned
    */
-  var resultIndex = 1;
+  search.resultIndex = 1;
   
   /**
    * Returns the application server object which by default is the application
@@ -28,27 +28,33 @@ jsx3.lang.Package.definePackage(
     var objXSL =  new jsx3.xml.Document();
     objXSL.load(search.getServer().resolveURI('xsl/findrecordid.xsl'));
     var objXML =  search.getServer().getCache().getDocument("cachedlist");
-    var params= {'searchedtext':strSearched, 'resultindex':resultIndex}
+    var params= {'searchedtext':strSearched, 'resultindex':this.resultIndex}
     var objPROC = new jsx3.xml.Template(objXSL);
     objPROC.setParams(params);
-    var strRECORDID = objPROC.transform(objXML);
+    var xmlRECORDID = objPROC.transform(objXML);
+    var strRECORDID = this.removeWrap(xmlRECORDID);
     jsx3.log("id = " + strRECORDID);
     if (strRECORDID){
-      resultIndex++;
+      this.resultIndex++;
       mylist.focusRowById(strRECORDID)
     }
     else{
-      var message = (resultIndex == 1)?'Text not found':'End of search'
-      search.resetResult();
+      var message = (this.resultIndex == 1)?'Text not found':'End of search'
+      search.resultIndex = 1; // reset search index
       search.getServer().alert(message,message)
     }
   } 
 
 /**
- * Resets the search index to 1
+ * 
  */
-  search.resetResult = function() {
-    resultIndex = 1 ;
+  search.removeWrap = function(wrapped) {
+    if (wrapped.indexOf("<transformiix:result") > -1) {
+       return wrapped.substring(wrapped.indexOf(">") + 1, 
+                                    wrapped.lastIndexOf("<"));
+    } else
+     return wrapped;
+
   }
 
-})
+});
