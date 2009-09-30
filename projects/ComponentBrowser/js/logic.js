ffd0783d2dbe5446d429255575b14a358c5d84b8
BryanForbes <BryanForbes@21de6678-13f1-11de-81b6-69408224c2c7>
2009-09-30 22:19:44 +0000

    !strict * Added component demo loader. * Added two demos which don't do anything right now. * Hooked up view source to actually view the source.

--- projects/ComponentBrowser/js/logic.js
+++ projects/ComponentBrowser/js/logic.js
@@ -40,29 +40,90 @@ jsx3.Package.definePackage("tibco.ce", function(ce){
   };
 
   ce.componentSelect = function(componentId) {
+    var component = ce.getJSXByName('demo_' + componentId);
+    if(component){
+      ce.viewComponent(componentId, component);
+      return;
+    }
+
     this.setView(ce.LOADING);
 
-    var self = this;
-    setTimeout(function(){
-      self.componentLoaded(componentId);
-    }, 1000);
+    var objCache = this.getCache();
+    var rsURL = this.resolveURI('components/demos/' + componentId + '.xml');
+
+    var doc = objCache.getDocument(componentId);
+
+    if (!doc) {
+      var loadFunc = function(objEvent) {
+        var Document = jsx3.xml.Document;
+        var doc = objCache.getDocument(objEvent.id);
+        var strEvtType = objEvent.response;
+        var strId = objEvent.id;
+
+        objCache.unsubscribe('load.' + componentId, loadFunc);
+
+        if (strEvtType == Document.ON_RESPONSE) {
+          ce.componentLoaded(componentId, doc);
+        } else if (strEvtType == Document.ON_TIMEOUT) {
+          ce.componentTimeout(componentId, doc);
+        } else if (strEvtType == Document.ON_ERROR) {
+          ce.componentError(componentId, doc);
+        }
+      };
+
+      objCache.subscribe('load.' + componentId, loadFunc);
+      doc = objCache.getOrOpenAsync(rsURL, componentId);
+    } else {
+      ce.componentLoaded(componentId, doc);
+    }
   };
 
-  ce.componentLoaded = function(componentId) {
-      var tree = ce.getJSXByName('treeExplorer');
-      var compName = ce.getJSXByName('panePropertiesName');
+  ce.componentLoaded = function(componentId, objXML) {
+    var demoView = ce.getJSXByName('componentDemoView');
+    demoView.loadXML(objXML, true);
 
-      var record = tree.getRecord(componentId);
-      var rNode = tree.getRecordNode(componentId);
-      var pNode = rNode.getParent();
+    ce.viewComponent(componentId);
+  };
 
-      var name = pNode.getAttribute("jsxtext");
+  ce.componentTimeout = function(componentId, objXML) {
+    console.log("TIMEOUT");
+  };
 
-      this.setView(ce.COMPONENT, name + " &raquo; " + record.jsxtext);
-      compName.setText(name, true);
+  ce.componentError = function(componentId, objXML) {
+    console.log("ERROR");
+  };
+
+  var _selectedComponent = null;
+  ce.viewComponent = function(componentId, component) {
+    if (_selectedComponent) {
+      _selectedComponent.setDisplay(jsx3.gui.Block.DISPLAYNONE, true);
+    }
+
+    var component = component || ce.getJSXByName('demo_' + componentId);
+
+    var tree = ce.getJSXByName('treeExplorer');
+    var compName = ce.getJSXByName('panePropertiesName');
+
+    var record = tree.getRecord(componentId);
+    var rNode = tree.getRecordNode(record.jsxid);
+    var pNode = rNode.getParent();
+
+    var name = pNode.getAttribute("jsxtext");
+
+    this.setView(ce.COMPONENT, name + " &raquo; " + record.jsxtext);
+    compName.setText(name, true);
+    component.setDisplay(jsx3.gui.Block.DISPLAYBLOCK, true);
+
+    _selectedComponent = component;
   };
 
   ce.viewSource = function(button) {
+    var componentId = _selectedComponent.jsxname.slice(5);
+    var doc = ce.getCache().getDocument(componentId);
+
+    var sourceBlock = ce.getJSXByName('sourceBlock');
+    sourceBlock.setText('<pre>' + doc.getXML().replace(/</g, '&lt;') + '</pre>', true);
+
     var layout = ce.getJSXByName('lytProperties');
     layout.setRows("*,0", true);
     layout = ce.getJSXByName('componentViewLayout');
