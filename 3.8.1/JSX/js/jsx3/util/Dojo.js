443ce53d9e8f5a63136ff93c8c45273556d6268f
jcg <jcg@21de6678-13f1-11de-81b6-69408224c2c7>
2009-10-09 18:15:41 +0000

    Tagging GI 3.8.1 GA !strict

---
+++ 3.8.1/JSX/js/jsx3/util/Dojo.js
@@ -0,0 +1,103 @@
+/**
+ * This provides a static instance that acts as a bridge to Dojo's publish/subscribe hub.
+ * Events on dojo's hub can be subscribed to through this instance, which implements the
+ * EventDispatcher interface. Events can also be published through this bridge, and they
+ * will be broadcast on Dojo's hub.
+ * @package
+*/
+jsx3.Class.defineClass("jsx3.util.DojoPubSub", null, [jsx3.util.EventDispatcher], function(DojoPubSub, DojoPubSub_prototype) {
+
+  var eventHandlers = {};
+  var defaultPublish = jsx3.util.EventDispatcher.prototype.publish;
+
+  DojoPubSub_prototype.publish = function(message) {
+    if (jsx3.util.Dojo.isLoaded()) {
+      message._fromGI = true;
+      dojo.publish(message.subject, [message]);
+      return defaultPublish.call(this, message);
+    }
+  };
+
+  DojoPubSub_prototype.subscribe = function(topic, context, method) {
+    if (jsx3.util.Dojo.isLoaded()) {
+      var self = this;
+      if (!eventHandlers[topic]) {
+        eventHandlers[topic] = dojo.subscribe(topic, null, function(m) {
+          if(!(m && m._fromGI)) {
+            var message = {subject: topic};
+            for (var i = 0; i < arguments.length; i++) {
+              message[i] = arguments[i];
+            }
+            defaultPublish.call(self, message);
+          }
+        });
+      }
+      return this.jsxsupermix(topic, context, method);
+    }
+  };
+
+});
+
+/**
+ * Provides the ability to load the Dojo JavaScript library.
+ *
+ * @see jsx3.gui.DojoWidget
+ * @see jsx3.xml.DojoDataStore
+ */
+jsx3.Class.defineClass("jsx3.util.Dojo", null, null, function(Dojo) {
+
+  /**
+   * {jsx3.util.EventDispatcher}
+   * A bridge to Dojo's publish/subscribe hub. Events on the Dojo hub can be subscribed to through this
+   * instance. Events can also be published through this bridge, and they will be broadcast on the Dojo hub.
+   * <code>Dojo.load()</code> must be called before this object can be used.
+   * @see #load()
+   */
+  Dojo.hub = new jsx3.util.DojoPubSub();
+
+  /**
+   * Returns the resolved path to Dojo or a file within Dojo. Dojo is assumed to be installed in the directory
+   * <code>dojo-toolkit/</code> as a peer of <code>JSX/</code>. This location may be overridden by setting the
+   * <code>jsx_dojo</code> deployment parameter.
+   *
+   * @param s {String} the relative path of a Dojo resource.
+   */
+  Dojo.getPath = function(s) {
+    var prefix = jsx3.getEnv("jsx_dojo") || "jsx:/../dojo-toolkit";
+    return jsx3.resolveURI(prefix + (s ? s : ""));
+  };
+
+  /**
+   * Loads the Dojo JavaScript library. Dojo must be installed at URI <code>jsx:/../dojo-toolkit</code> or the
+   * <code>jsx_dojo</code> deployment parameter must be set to the location where Dojo is installed.
+   */
+  Dojo.load = function() {
+    if (typeof dojo == "undefined") {
+      window.djConfig = typeof djConfig == "undefined" ? {baseUrl: jsx3.util.Dojo.getPath("/dojo/"), afterOnLoad: true} : djConfig;
+      // if we are running from the source version, we will pretend we are spidermonkey
+      // providing a load function, which will use the sync loader instead of the
+      // destructive document.write technique used by the source version of dojo.js
+
+      load = function(script){
+        jsx3.CLASS_LOADER.loadJSFileSync(script.replace(/rhino|spidermonkey/,"browser"));
+      };
+      jsx3.CLASS_LOADER.loadJSFileSync(jsx3.util.Dojo.getPath("/dojo/dojo.js"));
+      delete load;
+
+      // we need to redefine this so the JSXBODY element is treated as the body.
+      // This is done for Dijit's popup code so it gets styled correctly.
+      dojo.body = function(){
+        return dojo.query('[label="JSXBODY"]')[0];
+      };
+    }
+  };
+
+  /**
+   * Returns whether Dojo has been loaded.
+   * @return {boolean}
+   */
+  Dojo.isLoaded = function() {
+    return typeof dojo != "undefined";
+  };
+  
+});
---
+++ 3.8.1/test/tests/jsx3/t_pkg.js
@@ -0,0 +1,155 @@
+/*
+ * Copyright (c) 2001-2009, TIBCO Software Inc.
+ * Use, modification, and distribution subject to terms of license.
+ */
+
+gi.test.jsunit.defineTests("jsx3", function(t, jsunit) {
+
+  jsunit.require("jsx3.jsxpackage", "jsx3.app.Cache", "jsx3.lang.System");
+
+  t.testSharedCache = function() {
+    var c = jsx3.getSharedCache();
+    jsunit.assertInstanceOf(c, jsx3.app.Cache);
+  };
+
+  t.testSystemCache = function() {
+    var c = jsx3.getSystemCache();
+    jsunit.assertInstanceOf(c, jsx3.app.Cache);
+  };
+
+  t.testSleep1 = function() {
+    jsx3.sleep(t.asyncCallback(function() {
+      jsunit.assert(true);
+    }));
+  };
+  t.testSleep1._async = true;
+
+  t.testSleep2 = function() {
+    var obj = {};
+    jsx3.sleep(t.asyncCallback(function() {
+      jsunit.assertNotNullOrUndef(this);
+      jsunit.assertEquals(obj, this);
+    }), null, obj);
+  };
+  t.testSleep2._async = true;
+
+  t.testSleep3 = function() {
+    var firstExecuted = false, secondExecuted = false;
+
+    jsx3.sleep(function() {
+      firstExecuted = true;
+    }, "dupId");
+    jsx3.sleep(function() {
+      secondExecuted = true;
+    }, "dupId");
+
+    window.setTimeout(t.asyncCallback(function() {
+      jsunit.assertTrue(firstExecuted);
+      jsunit.assertFalse(secondExecuted);
+    }), 1000);
+  };
+  t.testSleep3._async = true;
+
+  t.testSleep4 = function() {
+    var firstExecuted = false, secondExecuted = false;
+
+    jsx3.sleep(function() {
+      firstExecuted = true;
+    }, "dupId");
+    jsx3.sleep(function() {
+      secondExecuted = true;
+    }, "dupId", null, true);
+
+    window.setTimeout(t.asyncCallback(function() {
+      jsunit.assertFalse(firstExecuted);
+      jsunit.assertTrue(secondExecuted);
+    }), 1000);
+  };
+  t.testSleep4._async = true;
+
+  t.testSleepThrows1 = function() {
+    var secondExecuted = false;
+    jsx3.sleep(function() {
+      jsx3.sleep(function() {
+        secondExecuted = true;
+      });
+
+      throw new Error();
+    });
+
+    window.setTimeout(t.asyncCallback(function() {
+      jsunit.assertTrue(secondExecuted);      
+    }), 1000);
+  };
+  t.testSleepThrows1._async = true;
+
+  t.testSleepThrows2 = function() {
+    var secondExecuted = false;
+    jsx3.sleep(function() {
+      throw new Error();
+    });
+    jsx3.sleep(function() {
+      secondExecuted = true;
+    });
+
+    window.setTimeout(t.asyncCallback(function() {
+      jsunit.assertTrue(secondExecuted);
+    }), 1000);
+  };
+  t.testSleepThrows2._async = true;
+
+  t.testRequire1 = function() {
+    if (jsx3.app && jsx3.app.UserSettings) delete jsx3.app.UserSettings;
+
+    if (jsx3.util)
+      jsunit.assertUndefined(jsx3.app.UserSettings);
+
+    jsx3.require("jsx3.app.UserSettings");
+
+    jsunit.assertNotUndefined(jsx3.app.UserSettings);
+    jsunit.assertInstanceOf(jsx3.app.UserSettings.jsxclass, jsx3.lang.Class);
+  };
+
+  t.testRequire2 = function() {
+    var e = null;
+    if (jsx3.app && jsx3.app.UserSettings) delete jsx3.app.UserSettings;
+
+    if (jsx3.app)
+      jsunit.assertUndefined(jsx3.app.UserSettings);
+
+    jsx3.sleep(function() {
+      jsx3.require("jsx3.app.UserSettings");
+      try {
+        jsunit.assertNotUndefined("Class jsx3.app.UserSettings should not be undefined (1): " + jsx3.app.UserSettings, jsx3.app.UserSettings);
+        jsunit.assertInstanceOf(jsx3.app.UserSettings.jsxclass, jsx3.lang.Class);
+      } catch (ex) {
+        e = ex;
+      }
+    });
+
+    if (e == null) {
+      jsx3.sleep(function() {
+        try {
+          jsunit.assertNotUndefined("Class jsx3.app.UserSettings should not be undefined (2): " + jsx3.app.UserSettings, jsx3.app.UserSettings);
+          jsunit.assertInstanceOf(jsx3.app.UserSettings.jsxclass, jsx3.lang.Class);
+        } catch (ex) {
+          e = ex;
+        }
+
+        if (e == null) {
+          try {
+            jsx3.require("jsx3.app.UserSettings");
+          } catch (ex) {
+            e = ex;
+          }
+        }
+      });
+    }
+    
+    jsx3.sleep(t.asyncCallback(function() {
+      if (e) throw e;
+    }));
+  };
+  t.testRequire2._async = true;
+
+});
