8ceb9e514fa8636063656533574500d03a867b48
BryanForbes <BryanForbes@21de6678-13f1-11de-81b6-69408224c2c7>
2009-10-14 15:54:10 +0000

    !strict * Changed component template to include right column with properties. * Changed component loader to accomodate new component demo format. * Added dynamic property for the color of the right hand column in the demos. * Changed blockrelative.xml to the new format.

--- projects/ComponentBrowser/js/logic.js
+++ projects/ComponentBrowser/js/logic.js
@@ -42,82 +42,62 @@ jsx3.Package.definePackage("tibco.ce", function(ce){
     lytContent.repaint();
   };
 
-  ce.componentSelect = function(tree, componentId) {
-    var component = ce.getJSXByName('demo_' + componentId);
-    if(component){
-      ce.viewComponent(componentId, component);
-      return;
-    }
+  var Document = jsx3.xml.Document;
+  ce._onAsyncDone = function(objEvent) {
+    var objXML = objEvent.target;
+    var strEvtType = objEvent.subject;
+    var componentId = objXML._componentId;
 
-    this.setView(ce.LOADING);
+    delete objXML._componentId;
+    objXML.unsubscribe("*", this);
 
-    var objCache = this.getCache();
-    var rsURL = this.resolveURI('components/demos/' + componentId + '.xml');
-
-    var doc = objCache.getDocument(componentId);
-
-    if (!doc) {
-      var loadFunc = function(objEvent) {
-        var Document = jsx3.xml.Document;
-        var doc = objCache.getDocument(objEvent.id);
-        var strEvtType = objEvent.response;
-        var strId = objEvent.id;
-
-        objCache.unsubscribe('load.' + componentId, loadFunc);
-
-        if (strEvtType == Document.ON_RESPONSE) {
-          ce.componentLoaded(componentId, doc);
-        } else if (strEvtType == Document.ON_TIMEOUT) {
-          ce.componentTimeout(componentId, doc);
-        } else if (strEvtType == Document.ON_ERROR) {
-          ce.componentError(componentId, doc.getAttribute('error'));
-        }
-      };
-
-      objCache.subscribe('load.' + componentId, loadFunc);
-      doc = objCache.getOrOpenAsync(rsURL, componentId);
-    } else {
-      ce.componentLoaded(componentId, doc);
+    if (strEvtType == Document.ON_RESPONSE) {
+      ce.componentLoaded(componentId, objXML);
+    } else if (strEvtType == Document.ON_TIMEOUT) {
+      ce.componentTimeout(componentId, objXML);
+    } else if (strEvtType == Document.ON_ERROR) {
+      ce.componentError(componentId, objXML.getError().toString());
     }
   };
 
+  ce.componentSelect = function(tree, componentId) {
+    this.setView(ce.LOADING);
+
+    var rsURL = this.resolveURI('components/demos/' + componentId + '.xml');
+
+    var doc = Document.jsxclass.newInstance();
+    doc.setAsync(true);
+
+    doc.subscribe('*', this, '_onAsyncDone');
+    doc._componentId = componentId;
+    doc.load(rsURL, 60000);
+  };
+
+  var _selectedComponentId = null;
   ce.componentLoaded = function(componentId, objXML) {
-    var demoView = ce.getJSXByName('componentDemoView');
-
-    var demoTab = new jsx3.gui.Tab(componentId + '_tab', componentId, null, null, null);
-    demoTab.getFirstChild()
-      .setBackgroundColor("#fffeff")
-      .setRelativePosition(1)
-      .setOverflow(3)
-      .setPadding('8 8 8 8', true);
-    demoView.setChild(demoTab, jsx3.app.Model.PERSISTNONE);
-    demoTab.getFirstChild().loadXML(objXML, true);
-    demoView.repaint();
-
-    var objCache = this.getCache();
-    var propDoc = objCache.getDocument(componentId + '_properties');
-
-    var properties = objXML.selectSingleNode('//t:serialization/properties', 'xmlns:t="urn:tibco.com/v3.0"');
-
-    if (properties && !propDoc) {
-      propDoc = new jsx3.xml.CDF.Document.newDocument();
-      var iter = properties.selectNodeIterator('//properties/data/record');
-      while (iter.hasNext()) {
-        var record = iter.next().cloneNode(true);
-        for (var i = record.selectNodeIterator("//enum"); i.hasNext(); ) {
-          var objNode = i.next();
-          var value = objNode.getAttribute('jsxid');
-          if (objNode.getAttribute("eval") !== "0")
-              value = jsx3.eval(value);
-          objNode.setAttribute('jsxvalue', value);
-        }
-        propDoc.appendChild(record);
-      }
-      objXML.removeChild(properties);
-      objCache.setDocument(componentId + '_properties', propDoc);
+    var container = ce.getJSXByName('componentViewContainer');
+    var root = container.getDescendantOfName('root');
+    if (root) {
+      container.removeChild(root);
     }
 
-    ce.viewComponent(componentId);
+    container.loadXML(objXML, true);
+
+    var tree = ce.getJSXByName('treeExplorer');
+
+    var record = tree.getRecord(componentId);
+    var rNode = tree.getRecordNode(record.jsxid);
+
+    var name = record.jsxtext;
+    if (!record.jsximg) {
+      name = rNode.getParent().getAttribute('jsxtext');
+    }
+
+    var source = ce.getJSXByName('sourceView');
+    source.setSourceXML(objXML);
+
+    _selectedComponentId = componentId;
+    this.setView(ce.COMPONENT, ce._getComponentTitle(rNode, record.jsxtext));
   };
 
   ce.componentTimeout = function(componentId) {
@@ -126,28 +106,6 @@ jsx3.Package.definePackage("tibco.ce", function(ce){
 
   ce.componentError = function(componentId, error) {
     ce.showError(error, "Error");
-  };
-
-  ce._updatePropertyNode = function(objNode, component) {
-    var getter = objNode.getAttribute("getter");
-    var propName = objNode.getAttribute("jsxprop") || objNode.getAttribute("jsxid");
-    var target = objNode.getAttribute("jsxtarget");
-    target = target ? component.getDescendantOfName(target) : _targetComponent;
-    var dynVal = target.getDynamicProperty(propName);
-
-    var stepVal = null;
-    if (getter) {
-      if (/^[_a-zA-Z]\w*$/.test(getter)) {
-        stepVal = target[getter]();
-      } else {
-        stepVal = target.eval(getter);
-      }
-    } else if (target[propName] != null) {
-      stepVal = target[propName];
-    }
-
-    objNode.setAttribute('value', stepVal);
-    objNode.setAttribute('jsxdynamic', dynVal);
   };
 
   ce._getComponentTitle = function(compNode, strName) {
@@ -162,73 +120,11 @@ jsx3.Package.definePackage("tibco.ce", function(ce){
     return strName;
   };
 
-  var _selectedComponent = null, _targetComponent = null;
-  ce.viewComponent = function(componentId, component) {
-    try {
-      var component = component || ce.getJSXByName('demo_' + componentId);
-      _targetComponent = component.getDescendantOfName('target');
-    } catch (e) {
-      ce.showError("Error with demo: " + componentId);
-      return;
-    }
-
-    var tabPane = ce.getJSXByName('componentDemoView');
-
-    var selIdx = tabPane.getSelectedIndex();
-    var tab = ce.getJSXByName(componentId + '_tab');
-
-    if (selIdx != tab.getChildIndex()) {
-      tabPane.setSelectedIndex(tab);
-    }
-
-    var propDoc = this.getCache().getDocument(componentId + '_properties');
-
-    var propEditor = ce.getJSXByName('propertiesMatrix');
-    var propEditorPane = ce.getJSXByName('propertiesMatrixPane');
-    var propEditorCont = ce.getJSXByName('propertiesMatrixContainer');
-    if (propDoc) {
-      var count = 0;
-      for (var i=propDoc.selectNodeIterator('//record'); i.hasNext(); ) {
-        count++;
-        ce._updatePropertyNode(i.next(), component);
-      }
-      propEditor.setSourceXML(propDoc);
-      // set the container block to the same height as the number of rows
-      // time the height of each row
-      propEditorCont.setHeight(count * 20);
-      propEditorPane.setDisplay(jsx3.gui.Block.DISPLAYBLOCK);
-      propEditorPane.repaint();
-    } else {
-      propEditorPane.setDisplay(jsx3.gui.Block.DISPLAYNONE);
-    }
-
-    var tree = ce.getJSXByName('treeExplorer');
-    var compName = ce.getJSXByName('panePropertiesName');
-
-    var record = tree.getRecord(componentId);
-    var rNode = tree.getRecordNode(record.jsxid);
-
-    var name = record.jsxtext;
-    if (!record.jsximg) {
-      name = rNode.getParent().getAttribute('jsxtext');
-    }
-
-    this.setView(ce.COMPONENT, ce._getComponentTitle(rNode, record.jsxtext));
-    compName.setText(name);
-
-    _selectedComponent = component;
-
-    this._updateSource();
-  };
-
-  var _sourceOpen = false;
   ce.viewSource = function(button) {
-    this._updateSource(true);
     var layout = ce.getJSXByName('lytProperties');
     layout.setRows("*,0", true);
     layout = ce.getJSXByName('componentViewLayout');
     layout.setRows("*,50%", true);
-    _sourceOpen = true;
   };
 
   ce.unViewSource = function() {
@@ -236,12 +132,6 @@ jsx3.Package.definePackage("tibco.ce", function(ce){
     layout.setRows("*,0", true);
     layout = ce.getJSXByName('lytProperties');
     layout.setRows("*,25", true);
-    _sourceOpen = false;
-  };
-
-  ce._updateSource = function(force) {
-    if (!_sourceOpen && !force) return;
-    this.getCache().setDocument('source_xml', _targetComponent.toXMLDoc());
   };
 
   ce.onCopySource = function (viewSource) {
@@ -257,152 +147,6 @@ jsx3.Package.definePackage("tibco.ce", function(ce){
   ce.onMouseOutSource = function(buttonNode) {
     // This is hooked up in the properties of the view source components
     buttonNode.style.backgroundColor = '#9898a5';
-  };
-
-  ce.onBeforePropertyEdit = function(objGrid, objColumn, strRecordId) {
-    if (objColumn != objGrid.getChild(1)) return false;
-
-    var objRecord = objGrid.getRecordNode(strRecordId);
-    var maskId = objRecord.getAttribute("jsxmask");
-    if (!maskId) return false;
-
-    var objMask = objColumn.getChild(maskId) || objColumn.getChild("jsxtextbox");
-
-    if (objMask instanceof jsx3.gui.Select) {
-      objMask.clearXmlData();
-
-      for (var i = objRecord.selectNodeIterator("enum"); i.hasNext(); ) {
-        var enumNode = i.next();
-        var value = enumNode.getAttribute('jsxvalue');
-        var text = enumNode.getAttribute('jsxtext');
-        objMask.insertRecord({jsxid:value, jsxtext:text});
-      }
-
-      objMask.redrawRecord(objMask.getValue()); // update the displayed text
-    }
-    return {objMASK:objMask};
-  };
-
-  ce.onAfterPropertyEdit = function(objGrid, strRecordId, strValue) {
-    //called when the edit mask is about to be committed (jsxafteredit)
-    var propRecord = objGrid.getRecord(strRecordId);
-
-    if (typeof(strValue) == "string")
-      strValue = jsx3.util.strTrim(strValue);
-
-    return ce._editObjectProperty(propRecord, propRecord.jsxprop||strRecordId, strValue);
-  };
-
-  ce.onPropertyMenuExecute = function(objMenu, strRecordId) {
-    //called when a menu item is selected; get its id; two are standard; all others are lookups
-    var oPE = ce.getJSXByName('propertiesMatrix');
-    var recordId = objMenu.getContextRecordId();
-    var objRecord = oPE.getRecordNode(recordId);
-    var strPropName = objRecord.getAttribute('jsxprop')||recordId;
-    var target = objRecord.getAttribute('jsxtarget');
-    target = target ? _selectedComponent.getDescendantOfName(target) : _targetComponent;
-    var strLookupId;
-
-    if (strRecordId == "jsxdpclear") {
-      target.setDynamicProperty(strPropName);
-      this._editObjectProperty(oPE.getRecord(recordId), strPropName, null);
-    }
-
-    this._updatePropertyNode(objRecord, _selectedComponent);
-
-    oPE.redrawRecord(recordId, jsx3.xml.CDF.UPDATE, oPE.getChild("propertiesValueColumn"));
-  };
-
-  ce._editObjectProperty = function(propRecord, propName, strValue) {
-    var strCheck = strValue != null ? strValue.toString() : "";
-
-    if (propRecord.disallow) {
-      var regex = propRecord.disallow.indexOf('/') == 0 ?
-          jsx3.eval(propRecord.disallow) : new RegExp(propRecord.disallow);
-      if (strCheck.match(regex)) {
-        ce.showError("input '" + jsx3.util.strEscapeHTML(strCheck) + "' for property " + propName + " is invalid, must not match " + regex, "Input Error");
-        return false;
-      }
-    }
-
-    if (propRecord.validate) {
-      var regex = propRecord.validate.indexOf('/') == 0 ?
-          jsx3.eval(propRecord.validate) : new RegExp(propRecord.validate);
-      if (! strCheck.match(regex)) {
-        ce.showError("input '" + jsx3.util.strEscapeHTML(strCheck) + "' for property " + propName + " is invalid, must match " + regex, "Validation Error");
-        return false;
-      }
-    }
-
-    // eval the new text if needed
-    if (propRecord["eval"] == "1") {
-      try {
-        strValue = this.eval(strValue);
-      } catch (e) {
-        ce.showError("error evaluating expression '" +
-            (strValue != null ? jsx3.util.strEscapeHTML(strValue) : null) + "': " + jsx3.NativeError.wrap(e));
-        return false;
-      }
-    }
-
-    // check for special on edit script
-    var changeScript = propRecord["jsxexecute"];
-    var target = propRecord["jsxtarget"];
-    target = target ? _selectedComponent.getDescendantOfName(target) : _targetComponent;
-
-    if (changeScript != null) {
-      // script context
-      try {
-        this.eval(changeScript, {vntValue:strValue, objJSX:target});
-      } catch (e) {
-        ce.showError("error evaluating expression '" + changeScript + "': " + jsx3.NativeError.wrap(e));
-        return false;
-      }
-    } else {
-      target[propName] = strValue;
-      target.repaint();
-    }
-
-    this._updateSource();
-
-    return true;
-  };
-
-  ce.makeSpy = function(objGrid, strRecordId) {
-    var node = objGrid.getRecordNode(strRecordId);
-    if (node == null) return null;
-
-    var desc = node.getAttribute("jsxtip");
-    var getter = node.getAttribute("docgetter");
-    if (getter && getter.indexOf("(") < 0)
-      getter += "()";
-    var setter = node.getAttribute("docsetter");
-    if (setter && setter.indexOf("(") < 0)
-      setter += "()";
-    var deflt = node.getAttribute("docdefault");
-
-    var dep = node.getAttribute("deprecated");
-
-    var strHTML = "";
-    strHTML += "<span class='jsx3ide_propsspy'>";
-
-    strHTML += "<div class='name" + (dep ? " deprecated" : "") + "'>" + node.getAttribute("jsxtext") + "</div>";
-
-    if (dep)
-      strHTML += "<div class='desc dep'><span class='title'>Deprecated.</span> " + (dep != "1" ? dep : "") + "</div>";
-
-    strHTML += "<div class='desc'>" + (desc || "<i>No description provided.</i>") + "</div>";
-    if (deflt)
-      strHTML += "<div class='deflt'><span class='title'>Default Value:</span> " + deflt + "</div>";
-    if (getter)
-      strHTML += "<div class='method'><span class='title'>Getter:</span> " + getter + "</div>";
-    if (setter)
-      strHTML += "<div class='method'><span class='title'>Setter:</span> " + setter + "</div>";
-    if (! node.getAttribute("docnoprop"))
-      strHTML += "<div class='prop'><span class='title'>Property:</span> " + node.getAttribute("jsxid") + "</div>";
-
-    strHTML += "</span>";
-    return strHTML;
   };
 
   ce.getErrorDialog = function() {
@@ -590,9 +334,7 @@ jsx3.Package.definePackage("tibco.ce", function(ce){
 
     var xmlDoc = this.getCache().getDocument('components_xml');
 
-    if (_searchBlank && _selectedComponent) {
-      var selectedId = _selectedComponent.jsxname.slice(5);
-
+    if (_searchBlank && _selectedComponentId) {
       // deselect the old selected component if there is one
       var oldSelected = xmlDoc.selectSingleNode('//record[@jsxselected="1"]');
       if (oldSelected) {
@@ -600,7 +342,7 @@ jsx3.Package.definePackage("tibco.ce", function(ce){
       }
 
       // select the currently selected component
-      var toSelect = xmlDoc.selectSingleNode('//record[@jsxid="' + selectedId + '"]');
+      var toSelect = xmlDoc.selectSingleNode('//record[@jsxid="' + _selectedComponentId + '"]');
       toSelect.setAttribute('jsxselected', '1');
 
       // open the currently selected demo's parent nodes
