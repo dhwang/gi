b68f2eba3f31bfc22c5912845eb22577c95b44d5
kzyp <kzyp@21de6678-13f1-11de-81b6-69408224c2c7>
2009-11-20 18:26:17 +0000

    Added launch.html template, refs GI-683

--- projects/PrototypeSharing/WEB-INF/jslib/settings.js
+++ projects/PrototypeSharing/WEB-INF/jslib/settings.js
@@ -1,7 +1,7 @@
 exports.MAIL = {
 	host:"mail.dojotoolkit.com",
 	defaultFrom: "prototype-admin@generalinterface.org"
-};
+}; 
 
 exports.DATABASE = {
 	connection:"jdbc:mysql://localhost/prototype?user=root&password=&useUnicode=true&characterEncoding=utf-8&autoReconnect=true",
@@ -9,11 +9,17 @@ exports.DATABASE = {
 };
 
 exports.CONFLUENCE = {
-	url: "http://localhost:8080/",
+	url: "http://localhost:8040/",
 	space: "ds",
-	username: "user",
-	password: "pass",
-	listingPageId: "32789"
+	username: "kriszyp",
+	password: "lacking",
+	listingPageId: "32789" 
 };
 
-exports.APACHE_TARGET = "/srv/www/htdocs/";
+exports.BYPASS_SECURITY= true;
+
+exports.APACHE_TARGET = "C:/wamp/www/";
+
+exports.HOST_URL_PREFIX = "http://localhost/";
+
+exports.DATA_FOLDER = "data"; // current directory is the default
