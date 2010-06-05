09e4f001294f8b512e1b7e1cebf7888e27a041ba
dhwang <dhwang@21de6678-13f1-11de-81b6-69408224c2c7>
2010-06-05 00:20:37 +0000

    Fixes GITAK-52; core missing version number, which is used in database reporting.

--- gitak/selenium/core/scripts/selenium-version.js
+++ gitak/selenium/core/scripts/selenium-version.js
@@ -1,5 +1,5 @@
-Selenium.version = "@VERSION@";
-Selenium.revision = "@REVISION@";
+Selenium.version = "1.0.0";
+Selenium.revision = "core-1.0.1;server-1.0.3";
 
 window.top.document.title += " v" + Selenium.version + " [" + Selenium.revision + "]";
 
