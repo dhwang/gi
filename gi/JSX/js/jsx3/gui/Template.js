8a155266bb128d871ca585b237c40e71087fecc9
xima <xima@21de6678-13f1-11de-81b6-69408224c2c7>
2013-07-23 06:22:23 +0000

    Fixes GI-975; Chrome/Safari 6/IE 10 , buttons inherit jsx3.gui.Template.Block display

--- gi/JSX/js/jsx3/gui/Template.js
+++ gi/JSX/js/jsx3/gui/Template.js
@@ -1596,9 +1596,13 @@ jsx3.Class.defineClass("jsx3.gui.Template.Box", jsx3.lang.Object, null, function
   /* @JSC */
   if (jsx3.CLASS_LOADER.SAF) {
     /** @private @jsxobf-clobber */
-    Box._CSS_FIXES = ["", "display:inline-table;", "", "display:inline-table;"];
+    Box._CSS_FIXES = ["", "display:inline-block;", "", "display:inline-block;"];
     /* @JSC */
   } else if (jsx3.CLASS_LOADER.IE) {
+    if (html.getMode() == html.MODE_FF_QUIRKS) // IE10 quirks (not IE10 in IE5 quirks) simulates Firefox quirks
+    /** @private @jsxobf-clobber */
+      Box._CSS_FIXES = ["", "display:inline-block;", "display:inline-block;", ""];
+    else
     /** @private @jsxobf-clobber */
     Box._CSS_FIXES = ["", "", "display:inline-block;", ""];
     /* @JSC */
