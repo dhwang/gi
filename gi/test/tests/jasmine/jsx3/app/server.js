5c0b02b86be6755bbbd4c9f539fab72fce1fb327
pkakodka <pkakodka@21de6678-13f1-11de-81b6-69408224c2c7>
2013-07-23 21:46:24 +0000

    Jasmine Tests: Fixing server.js as per code review

--- gi/test/tests/jasmine/jsx3/app/server.js
+++ gi/test/tests/jasmine/jsx3/app/server.js
@@ -1,22 +1,20 @@
 /*
-* Copyright (c) 2001-2013, TIBCO Software Inc.
-* Use, modification, and distribution subject to terms of license.
-*/
+ * Copyright (c) 2001-2013, TIBCO Software Inc.
+ * Use, modification, and distribution subject to terms of license.
+ */
 
-describe("jsx3.app.Server", function() {
+describe("jsx3.app.Server", function () {
   var _jasmine_test = gi.test.jasmine;
   _jasmine_test.require("jsx3.app.Server");
   var t = new _jasmine_test.TestSuite("jsx3.app.Server");
-
 
   beforeEach(function () {
     this.addMatchers(gi.test.jasmine.matchers);
     t._server = null;
   });
 
-  it("should be able to find the app.server namespace.", function() {
+  it("should be able to find the app.server namespace.", function () {
     var s = t._server = t.newServer("data/server1.xml", ".");
-
     expect(s.getEnv("namespace")).toBe("gi.test.App1");
     expect(s).toEqual(gi.test.App1); // gi.test.App1 is available globally
     expect(s).toBeInstanceOf("jsx3.app.Server");
@@ -24,23 +22,21 @@ describe("jsx3.app.Server", function() {
   });
 
   it("should retrieve the environment value stored in app.server.", function () {
-    var s = t._server = t.newServer("data/server1.xml", ".", null, {testkey:"testvalue"});
-
+    var s = t._server = t.newServer("data/server1.xml", ".", null, {testkey: "testvalue"});
     expect(s.getEnv("testkey")).toBe("testvalue");
     expect(s.getEnv("TestKey")).toBe("testvalue");// case insensitive
   });
 
-  it ("should show that environment key is case insensitive", function() {
-    var s = t._server = t.newServer("data/server1.xml", ".", null, {TestKey:"testvalue"}); // Case "TestKey"
-
+  it("should show that environment key is case insensitive", function () {
+    var s = t._server = t.newServer("data/server1.xml", ".", null, {TestKey: "testvalue"}); // Case "TestKey"
     expect(s.getEnv("testkey")).toBe("testvalue"); // case insensitive
     expect(s.getEnv("TestKey")).toBe("testvalue");
   });
 
-  afterEach(function() {
+  afterEach(function () {
     if (t._server) {
-     t._server.destroy();
-     delete t._server;
+      t._server.destroy();
+      delete t._server;
     }
   });
 });
