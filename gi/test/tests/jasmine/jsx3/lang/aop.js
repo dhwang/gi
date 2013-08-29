a06721fc4ec3e37aba56bd6c7703807e560d252d
xima <xima@21de6678-13f1-11de-81b6-69408224c2c7>
2013-08-29 02:32:31 +0000

    reverted the logic change and kept the description changes.

--- gi/test/tests/jasmine/jsx3/lang/aop.js
+++ gi/test/tests/jasmine/jsx3/lang/aop.js
@@ -3,7 +3,7 @@
  * Use, modification, and distribution subject to terms of license.
  */
  
- describe("jsx3.lang.AOP;The AOP class allows aspect oriented programming technique such as wrapping existing function in before call and after call code execution", function(){
+describe("jsx3.lang.AOP;The AOP class allows aspect oriented programming technique such as wrapping existing function in before call and after call code execution", function(){
 	var _jasmine_test = gi.test.jasmine;
 	_jasmine_test.require("jsx3.lang.AOP");
 	
@@ -14,47 +14,27 @@
 	});
 	
 	it("has method pc() that creates a new pointcut", function(){
-		var c1 = false, c2 = false, c3 = false;
-    
-		jsx3.lang.Class.defineClass("test.C1", null, null, function(C, P){
-			P.m = function() {
-				c1 = true;
-			};
-		});
-
-		jsx3.lang.Class.defineClass("test.C2", test.C1, null, function(C, P){
-			P.m = function() {
-				c2 = true;
-			};
-		});
-		
-		jsx3.lang.AOP.pc("testSubclass", {classes:"test.C2", methods:"m"});
-    
-    (new test.C2()).m();
-		expect(c1).toBeFalsy();
-		expect(c2).toBeTruthy();
-		
-		jsx3.lang.AOP.pcrem("testSubclass");
-		delete test.C1;
-		delete test.C2;
-	});
-  	
-	it("has method before() that add advice before any call to an instance method of a GI class", function(){
 		var c1 = false, c2 = false;
 		jsx3.lang.Class.defineClass("test.AOP", null,null ,function(C,P){
-			P.m = function(){
+			P.m = function(a,b,c){
 				expect(c2).toBeTruthy();
+				expect(a).toEqual(1);
+				expect(b).toEqual(2);
+				expect(c).toEqual(3);
 				c1 = true;
 			}
 		});
-   
+		
 		jsx3.lang.AOP.pc("testBefore", {classes:"test.AOP", methods:"m"});
-		jsx3.lang.AOP.before("testBefore",function(){
+		jsx3.lang.AOP.before("testBefore",function(a, b, c){
 			expect(c1).toBeFalsy();
+			expect(a).toEqual(1);
+			expect(b).toEqual(2);
+			expect(c).toEqual(3);
 			c2 = true;
 		});
 		
-		(new test.AOP()).m();
+		(new test.AOP()).m(1,2,3);
 		
 		expect(c1).toBeTruthy();
 		expect(c2).toBeTruthy();
@@ -67,8 +47,10 @@
 		var c1 = false, c2 = false;
 		expect(test.AOP).not.toBeDefined();
 		jsx3.lang.Class.defineClass("test.AOP", null, null, function(C,P){
-			P.m = function(){
+			P.m = function(a, b){
 				expect(c2).toBeFalsy();
+				expect(a).toEqual(1);
+				expect(b).toEqual(2);
 				c1 = true;
 				return 10;
 			};
@@ -76,14 +58,16 @@
 		
 		jsx3.lang.AOP.pc("testAfter",{classes:"test.AOP", methods:"m"});
 		
-		jsx3.lang.AOP.after("testAfter", function(rv){
+		jsx3.lang.AOP.after("testAfter", function(rv, a, b){
 			expect(c1).toBeTruthy();
+			expect(a).toEqual(1);
+			expect(b).toEqual(2);
 			expect(rv).toEqual(10);
 			c2 = true;
 			return 20;
 		});
 		
-		var v = (new test.AOP()).m();
+		var v = (new test.AOP()).m(1, 2);
 		expect(c1).toBeTruthy();
 		expect(c2).toBeTruthy();
 		expect(v).toEqual(10);
@@ -105,7 +89,6 @@
 			expect(a).toEqual(1);
 			expect(b).toEqual(2);
 			var rv = aop.proceed(a, 5);
-      //call an instance method
 			expect(rv).toEqual(10);
 			return 20;
 		});
@@ -115,5 +98,36 @@
 		
 		jsx3.lang.AOP.pcrem("testAround");
 		delete test.C1;
+	});
+	
+	it("should be able to create new point cut and before method before an inherited method", function(){
+		var c1 = false, c2 = false, c3 = false;
+    
+		jsx3.lang.Class.defineClass("test.C1", null, null, function(C, P){
+			P.m = function() {
+				c1 = true;
+			};
+		});
+
+		jsx3.lang.Class.defineClass("test.C2", test.C1, null, function(C, P){
+			P.m = function() {
+				c2 = true;
+			};
+		});
+		
+		jsx3.lang.AOP.pc("testSubclass", {classes:"test.C2", methods:"m"});
+		jsx3.lang.AOP.before("testSubclass", function() {
+			c3 = true;
+		});
+		
+		
+		(new test.C2()).m();
+		expect(c1).toBeFalsy();
+		expect(c2).toBeTruthy();
+		expect(c3).toBeTruthy();
+		
+		jsx3.lang.AOP.pcrem("testSubclass");
+		delete test.C1;
+		delete test.C2;
 	});
  });
