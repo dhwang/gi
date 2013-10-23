1ef511d25a41926a2e1748b051c6fa605ebd19f3
xima <xima@21de6678-13f1-11de-81b6-69408224c2c7>
2013-10-23 07:08:23 +0000

    Refs GI-982; Access is denied while open new window - set a global variable to store the new window

--- gitak/samples/gi36/sample-windowSwap/js/logic.js
+++ gitak/samples/gi36/sample-windowSwap/js/logic.js
@@ -26,6 +26,7 @@ jsx3.Package.definePackage("eg.windowswap", function(windowswap) {
 
       //and show the window if it isn'talready visible
       if (!w.isOpen()) {
+        window._childjsxwindow = w;
         w.open();
       }
       w.focus();
