6b4bb4519c50784f0de8a8aac869a26ddf845100
xima <xima@21de6678-13f1-11de-81b6-69408224c2c7>
2014-01-14 10:04:12 +0000

    jamsine test-- mmodified the suite.js

--- gi/test/tests/jasmine/suite.js
+++ gi/test/tests/jasmine/suite.js
@@ -6,13 +6,6 @@
 var specs = [
   "jsx3/ext.js", "jsx3/pkg.js",
 
-  /* GUI suite */
-  "jsx3/gui/nativeform.js",
-  "jsx3/gui/layoutgrid.js", "jsx3/gui/numberinput.js", 
-  "jsx3/gui/block.js", "jsx3/gui/gui_layout.js", "jsx3/gui/select.js", "jsx3/gui/textbox.js","jsx3/gui/cdf.js",
-  "jsx3/gui/button.js","jsx3/gui/checkbox.js","jsx3/gui/colorPicker.js","jsx3/gui/datePicker.js","jsx3/gui/dialog.js",
-  "jsx3/gui/timePicker.js", "jsx3/gui/radioButton.js", "jsx3/gui/slider.js", "jsx3/gui/matrix.js", "jsx3/gui/label.js",
-
   /* Reflection and metadata suite */
   "jsx3/lang/pkg.js", "jsx3/lang/object.js", "jsx3/lang/method.js", "jsx3/lang/class.js",
   "jsx3/lang/package.js", "jsx3/lang/aop.js",
@@ -43,7 +36,12 @@ var specs = [
   /* Localization suite */
   "jsx3/util/dateformat.js", "jsx3/util/locale.js", "jsx3/util/numberformat.js", "jsx3/util/messageformat.js",
 
-  
+  /* GUI suite */
+  "jsx3/gui/nativeform.js",
+  "jsx3/gui/layoutgrid.js", "jsx3/gui/numberinput.js", 
+  "jsx3/gui/block.js", "jsx3/gui/gui_layout.js", "jsx3/gui/select.js", "jsx3/gui/textbox.js","jsx3/gui/cdf.js",
+  "jsx3/gui/button.js","jsx3/gui/checkbox.js","jsx3/gui/colorPicker.js","jsx3/gui/datePicker.js","jsx3/gui/dialog.js",
+  "jsx3/gui/timePicker.js", "jsx3/gui/radioButton.js", "jsx3/gui/slider.js", "jsx3/gui/matrix.js", "jsx3/gui/label.js"  
 
 ];
 
