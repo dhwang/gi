0f6eb6d4b626355867459fbe9ed429556abe5b81
keli <keli@21de6678-13f1-11de-81b6-69408224c2c7>
2014-03-31 06:24:14 +0000

    refs GI-1009;added documentMode to judge the browser

--- gi/JSX/addins/amp/js/jsx3/amp/Engine.js
+++ gi/JSX/addins/amp/js/jsx3/amp/Engine.js
@@ -1139,7 +1139,7 @@ jsx3.lang.Class.defineClass("jsx3.amp.Engine", null, [jsx3.util.EventDispatcher]
           }
           break;
         case "css":
-          if (jsx3.CLASS_LOADER.IE) {
+          if (jsx3.CLASS_LOADER.IE &&(!document.documentMode || document.documentMode <=10)) {
             var styleNode = document.createElement("style");
             styleNode.setAttribute("type", "text/css");
             document.getElementsByTagName("head")[0].appendChild(styleNode);
