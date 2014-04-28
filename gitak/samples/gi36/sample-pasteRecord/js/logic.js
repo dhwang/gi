0433f542e3f0101f1b9c8ae2206e15724016d954
dhwang <dhwang@21de6678-13f1-11de-81b6-69408224c2c7>
2014-04-28 23:06:14 +0000

    Fix sample-pasteRecord to work in all browsers

--- gitak/samples/gi36/sample-pasteRecord/js/logic.js
+++ gitak/samples/gi36/sample-pasteRecord/js/logic.js
@@ -2,65 +2,83 @@ jsx3.lang.Package.definePackage(
   "eg.copypaste",                //the full name of the package to create
   function(copypaste) {          //name the argument of this function
 
-    copypaste.g_strRecordId = null;
+  copypaste.g_strRecordId = null;
 
-
-copypaste.getServer = function() {
+  copypaste.getServer = function() {
     return eg.copypaste.SERVER;
-}
-
-copypaste.showSpy = function(objList,strRecordId){
-  copypaste.g_strRecordId = strRecordId;
-  return  this.formatRecord(objList.getRecord(strRecordId));
-}
-
-
-copypaste.formatRecord = function(objRecord){
-  var output = "";
-  for (var p in objRecord){
-    if (p.substring(0,3) != 'jsx') {
-      output += p + ": " + objRecord[p] + "\r\n";
-    }
   }
-  return output;
-}
 
-// Called by context menu to copy
-copypaste.copyRecordToClipboard = function(menu){
-  //find source jsx3.gui.Matrix object from menu context parent
-  var sList = menu.getContextParent();
+  copypaste.showSpy = function(objList, strRecordId){
+    copypaste.g_strRecordId = strRecordId;
+    return  this.formatRecord(objList.getRecord(strRecordId));
+  }
 
-  var strRecordId = sList.getValue();
 
-  if (strRecordId && strRecordId.length) {
-    var copytext=[];
-    for (var i=0; i < strRecordId.length; i++) {
-       copytext.push(copypaste.formatRecord(sList.getRecord(strRecordId[i])));
+  copypaste.formatRecord = function(objRecord){
+    var output = "";
+    for (var p in objRecord){
+      if (p.substring(0,3) != 'jsx') {
+        output += p + ": " + objRecord[p] + "\r\n";
+      }
     }
-    jsx3.html.copy(copytext.join());
-  } else if (!strRecordId) {
-    var record = sList.getRecord(strRecordId);
-    // cross-browser copy
-    jsx3.html.copy(copypaste.formatRecord(record));
-  }  else {
-    //try to use record id from spy
-    jsx3.log('last spy = ' + copypaste.g_strRecordId);
-    var record = sList.getRecord(copypaste.g_strRecordId);
-    // cross-browser copy
-    jsx3.html.copy(copypaste.formatRecord(record));
+    return output;
+  }
+
+  // There's no system clipboard access on platform other than IE, 
+  // so we directly set value from copy.
+  copypaste.paste = function() {
+      // cross-browser copy
+      if (window.clipboardData) // clipboardData only exist on IE
+        this.pasteFromClipboard()
+      else
+        this.getServer().getJSX('textarea').setValue(copypaste.copyRecordToClipboard()); 
+    return true;
+  }
+
+  // Called by context menu to copy
+  // Also called by paste when cliboardData doesn't exist
+
+  copypaste.copyRecordToClipboard = function(menu){
+    //find source jsx3.gui.Matrix object from menu context parent
+    var sList = (!menu) ? this.getServer().getJSX('list'): menu.getContextParent();
+    var cptext; // joined text
+    var strRecordId = sList.getValue();
+
+    if (strRecordId && strRecordId.length) {
+      var copytext=[];
+      for (var i=0; i < strRecordId.length; i++) {
+         copytext.push(copypaste.formatRecord(sList.getRecord(strRecordId[i])));
+      }
+      cptext = copytext.join(); 
+
+    } else if (!strRecordId) {
+      var record = sList.getRecord(strRecordId);
+      // cross-browser copy
+      cptext = copypaste.formatRecord(record);
+
+    }  else {
+      //try to use record id from spy
+      jsx3.log('last spy = ' + copypaste.g_strRecordId);
+      var record = sList.getRecord(copypaste.g_strRecordId);
+      // cross-browser copy
+      cptext = copypaste.formatRecord(record);    
+    }
     
-  }
-   
-}
+    if (window.clipboardData) {
+      jsx3.html.copy(cptext);
+  	}
 
-copypaste.pasteFromClipboard = function(){
-  copypaste.getServer().getJSXByName("textarea").setValue(jsx3.html.paste(), true);
+    return cptext;
+  };
 
-}
+  copypaste.pasteFromClipboard = function(){
+    copypaste.getServer().getJSXByName("textarea").setValue(jsx3.html.paste(), true);
 
-copypaste.showSource = function(){
-  copypaste.getServer().getBodyBlock().load("components/javascriptSourceDialog.xml",true);
-}
+  };
+
+  copypaste.showSource = function(){
+    copypaste.getServer().getBodyBlock().load("components/javascriptSourceDialog.xml",true);
+  };
 
 
-})
+});
